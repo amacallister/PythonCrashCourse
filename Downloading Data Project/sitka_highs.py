fdf4f80798ff8551aafb3dbc0f0e0ea95f27ac93
amanda.macallister <amanda.macallister@docusign.com>
2024-05-14 14:58:02 -0500

    plotted highs and dates

---
+++ Downloading Data Project/sitka_highs.py
@@ -0,0 +1,35 @@
+"""Import the high and low temperatures from Sitka, Alaska in 2021."""
+
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+FILENAME = "data/sitka_weather_2021_simple.csv"
+with open(FILENAME, encoding="utf-8") as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # Get dates and high temperatures from this file.
+    dates, highs = [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        high = int(row[4])
+        dates.append(current_date)
+        highs.append(high)
+
+print(f"Highs: {highs}")
+print(f"Dates: {dates}")
+
+# Plot the high temperatures.
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c="red")
+
+# Format plot.
+ax.set_title("Daily high temperatures, - 2021", fontsize=24)
+ax.set_xlabel("", fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis="both", which="major", labelsize=16)
+
+plt.show()
