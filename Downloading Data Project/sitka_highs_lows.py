cca906b1bb82c7684fadd1bfe9c97af5d3a1d8d6
amanda.macallister <amanda.macallister@docusign.com>
2024-05-15 15:14:55 -0500

    added shading between high and low values in chart

--- Downloading Data Project/sitka_highs_lows.py
+++ Downloading Data Project/sitka_highs_lows.py
@@ -25,8 +25,9 @@ print(f"Dates: {dates}")
 
 # Plot the high and low temperatures.
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c="red")
-ax.plot(dates, lows, c="blue")
+ax.plot(dates, highs, c="red", alpha=0.5)
+ax.plot(dates, lows, c="blue", alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
 # Format plot.
 ax.set_title("Daily high and low temperatures, - 2021", fontsize=24)
